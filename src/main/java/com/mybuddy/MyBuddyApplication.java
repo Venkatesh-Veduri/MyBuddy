a72adab3982bc63a1cb571b19057c47d3a5e19f5
Venkates Veduri <venky.cse382@gmail.com>
2020-01-16 10:15:21 +0530

    initil Commit

---
+++ src/main/java/com/mybuddy/MyBuddyApplication.java
@@ -0,0 +1,13 @@
+package com.mybuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBuddyApplication.class, args);
+	}
+
+}
